50a5bc2422f1b8c7cf6e137037373483c0da8a1e
MaryKalantay <marykalantay@MaryKalantay.local>
2019-07-14 12:52:05 +0300

    W3C-98

---
+++ W3C_88/main.js
@@ -0,0 +1,8 @@
+function checkNumbers(n1, n2, d) {
+    n1 == n2;
+}
+
+console.log(newArray(2, 12, 4))
+
+
+
---
+++ W3C_98/main.js
@@ -0,0 +1,8 @@
+function checkNumbers(str) {
+    return str.toUpperCase() 
+}
+
+console.log(checkNumbers("Write a JavaScript"))
+
+
+
