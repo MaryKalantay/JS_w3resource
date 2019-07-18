272387b7af340a9f11f8afbb44a4b7090a4fd45b
MaryKalantay <marykalantay@MaryKalantay.local>
2019-07-18 09:40:12 +0300

    W3C-128

---
+++ W3C_128/main.js
@@ -0,0 +1,15 @@
+function rangeNumber(a, n,  b) {
+    if ( (n >= a) &&  (n <= b) ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(rangeNumber(12, 13, 34));
+
+
+
+
+
+
