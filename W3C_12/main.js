aeb6e7860560d216dd9e6a546ff08913bb24f04e
MaryKalantay <marykalantay@MaryKalantay.local>
2019-07-23 10:25:40 +0300

    W3C-50

--- W3C_12/main.js
+++ W3C_12/main.js
@@ -1,5 +1,5 @@
 function url() {
-    return window.location.href;;
+    return window.location.href;
 }
 
 console.log(url());
---
+++ W3C_50/main.js
@@ -0,0 +1,11 @@
+function findSymbol(str) {
+    return str.match(/[A-Z]/g).join('')
+}
+
+console.log(findSymbol("Masha, Masha, Masha, Masha"));
+
+
+
+
+
+
